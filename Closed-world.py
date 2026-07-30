b9a38792141f5e7bc15f59b7ea828fb63da40dab
Songyang Wu <wusongyang02@gmail.com>
2026-07-30 16:55:17 +0800

    Training and test in closed-world scenario

--- Closed-world.py
+++ Closed-world.py
@@ -263,10 +263,10 @@ def benchmark_inference(model: nn.Module, loader: DataLoader, device, warmup=30,
 # ================================
 def run():
     # ====== Data paths (please adjust to your own paths) ======
-    train_home_npz = "./Datasets/Chrome_Homepage_Single.npz"
-    sub_with_home_npz = "./data/Chrome_Subpage_Single.npz"
-    val_npz = "./Datasets/Chrome_Val.npz"
-    test_npz = "./Datasets/Chrome_Test.npz"
+    train_home_npz = "./Datasets/Chrome_Homepage_Single.npz"    # or Tor_Homepage_Single.npz
+    sub_with_home_npz = "./data/Chrome_Subpage_Single.npz"    # or Tor_Subpage_Single.npz
+    val_npz = "./Datasets/Chrome_Val.npz"    # or Tor_Val.npz
+    test_npz = "./Datasets/Chrome_Test.npz"    # or Tor_Test.npz
 
     save_dir = "./runs_FreeTab"
     os.makedirs(save_dir, exist_ok=True)
